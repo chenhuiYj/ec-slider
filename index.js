9b0fdd805e35278298c2d6c0f65835ac81415e83
chui-web <158874645@qq.com>
2018-11-22 15:40:44 +0800

    更换图片路径

--- index.js
+++ index.js
@@ -2,38 +2,45 @@ require("./index.html");
 require("./src/sass/index.scss");
 require("./fontSize");
 import Vue from 'vue'
+import ecDialog from 'ec-dialog'
 import ecslide from './src/js/components/index';
 Vue.use(ecslide);
+Vue.use(ecDialog);
+//let img=require('/dist/images/img1.jpg');
 let app6 = new Vue({
     el: "#app6",
     data: {
+        name:'chen',
         isTrue: [false],
         list: [
             {
-                src: 'http://i2.kiimg.com/1949/dc149abbf35fe1f3.jpg',
+                src: 'http://thyrsi.com/t6/613/1542871409x2890211738.jpg',
                 href:'https://www.baidu.com'
             },
             {
-                src: "http://i2.kiimg.com/1949/9906f77c822ab42d.jpg",
+                src: "http://thyrsi.com/t6/613/1542871429x2890211738.jpg",
                 href:'https://www.baidu.com'
             },
             {
-                src: "http://i2.kiimg.com/1949/92d88e0ff8edee1a.jpg",
+                src: "http://thyrsi.com/t6/613/1542871442x2890211738.jpg",
                 href:'https://www.baidu.com'
             },
             {
-                src: "http://i2.kiimg.com/1949/2a7d10e224cfaa8e.jpg",
+                src: "http://thyrsi.com/t6/613/1542871463x2890211738.jpg",
             }
         ],
         list2: [
             {
-                src: 'http://i2.kiimg.com/1949/dc149abbf35fe1f3.jpg',
+                src: 'http://thyrsi.com/t6/613/1542871429x2890211738.jpg',
                 href:'https://www.baidu.com'
             }
         ]
     },
     mounted(){
         this.isTrue[0] = true;
+        this.$ec_alert({
+            content:'提示的内容'
+        })
     }
 
 });
